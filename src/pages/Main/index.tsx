2948b224c243e083495726dad4775aa853554f19
Leoodaviid <david_ax7@hotmail.com>
2023-03-17 22:27:05 -0300

    recebendo dados no componente

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,10 +1,22 @@
+import { useEffect, useState } from 'react'
 import { Temperature } from '../../components/Temperature/Temperature'
+import { ClimateData } from '../../models/climateData'
+import { getClimate } from '../../services/climate'
 import { Container } from './styles'
 
 export default function Main() {
+  const [data, setData] = useState<ClimateData>()
+
+  useEffect(() => {
+    ;(async () => {
+      const dataRequest = await getClimate()
+      setData(dataRequest.data)
+    })()
+  }, [])
+
   return (
     <Container>
-      <Temperature />
+      <Temperature data={data} />
     </Container>
   )
 }
