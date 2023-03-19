cc68150e987d4e0b532b632b574535560c04f032
Leoodaviid <david_ax7@hotmail.com>
2023-03-19 20:42:43 -0300

    AirQuality renderizando apenas o item em map

--- src/components/AirQuality/AirQuality.tsx
+++ src/components/AirQuality/AirQuality.tsx
@@ -1,6 +1,5 @@
-import { Container, Title } from './styles'
-import Leaf from '../../assets/icons/leaf.svg'
 import { QualityData } from '../../models/climeData'
+import { AirQualityItem } from './AirQualityItem'
 
 interface AirQualityProps {
   quality: QualityData
@@ -9,54 +8,8 @@ interface AirQualityProps {
 export const AirQuality = ({ quality }: AirQualityProps) => {
   return (
     <>
-      {quality.list.map((quality) => (
-        <Container key={quality.main.aqi}>
-          <Title>
-            <img src={Leaf} alt='icone de folha' />
-            Qualidade do ar
-          </Title>
-          {quality.main.aqi === 1 ? (
-            <p className='qualitative'>Bom</p>
-          ) : quality.main.aqi === 2 ? (
-            <p className='qualitative'>Regular</p>
-          ) : quality.main.aqi === 3 ? (
-            <p className='qualitative'>Moderado</p>
-          ) : quality.main.aqi === 4 ? (
-            <p className='qualitative'>Ruim</p>
-          ) : quality.main.aqi === 5 ? (
-            <p className='qualitative'>Muito Ruim</p>
-          ) : (
-            <p className='qualitative'></p>
-          )}
-
-          <p className='number'>{quality.main.aqi}</p>
-          <div className='info'>
-            <div className='number'>
-              <p>{quality.components.pm2_5}</p>
-              <small>PM2.5</small>
-            </div>
-            <div className='number'>
-              <p>{quality.components.pm10}</p>
-              <small>PM10</small>
-            </div>
-            <div className='number'>
-              <p>{quality.components.so2}</p>
-              <small>SO₂</small>
-            </div>
-            <div className='number'>
-              <p>{quality.components.no2}</p>
-              <small>NO₂</small>
-            </div>
-            <div className='number'>
-              <p>2{quality.components.o3.toFixed()}</p>
-              <small>O₃</small>
-            </div>
-            <div className='number'>
-              <p>{quality.components.co.toFixed()}</p>
-              <small>CO</small>
-            </div>
-          </div>
-        </Container>
+      {quality.list.map((item) => (
+        <AirQualityItem key={item.dt} item={item} />
       ))}
     </>
   )
